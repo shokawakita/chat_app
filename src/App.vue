44998dbed74017b1bc4c2691a606d88b4fc9b047
shokawakita <shokawakita0630@gmail.com>
2024-10-20 13:32:20 +0900

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+    <HelloWorld />
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
