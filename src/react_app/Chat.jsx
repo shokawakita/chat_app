368e538576bada47cbb9a520668ef9bf8ef668b7
shokawakita <shokawakita0630@gmail.com>
2024-10-23 08:51:08 +0900

    add Chat.jsx

--- src/react_app/Chat.jsx
+++ src/react_app/Chat.jsx
@@ -4,6 +4,15 @@ import withSendbird from "@sendbird/uikit-react/withSendbird";
 import sendbirdSelectors from "@sendbird/uikit-react/sendbirdSelectors";
 const Chat = (props) => {
     const sdk = sendbirdSelectors.getSdk(props);
+    const currentUser = sdk && sdk.currentUser;
+    if (currentUser) {
+        const getTotalUnreadMessageCount = async () => {
+            const unreadMessageCount = await sdk.groupChannel.getTotalUnreadMessageCount();
+            props.setUnreadMessageCount(unreadMessageCount);
+        }
+        getTotalUnreadMessageCount();
+        props.setSbUserInfo(currentUser);
+    }
     return (<SendbirdApp appId={props.config.APP_ID} userId={props.config.USER_ID} />);
 }
 const ChatWithSendbird = withSendbird(Chat)
