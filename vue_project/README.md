3939f47d4d6b313327d99fe8ee60b913f850be00
shokawakita <shokawakita0630@gmail.com>
2024-10-24 07:30:28 +0900

    exchange all

--- vue_project/README.md
+++ vue_project/README.md
@@ -1,4 +1,4 @@
-# portfolio
+# vue_project
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vue_project/src/components/HelloWorld.vue
+++ vue_project/src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-// defineProps<{
-//   msg: string
-// }>()
-const message = ref(1)
+defineProps<{
+  msg: string
+}>()
 </script>
 
 <template>
-    <h1>{{ message }}</h1>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
 </template>
 
 <style scoped>
